772addd332d686b5f187c28c6e526cd9e6826bf2
田海鹏 <83957234+thp521@users.noreply.github.com>
2022-10-13 21:25:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -201,7 +201,7 @@ def tip():
         return "","","","","","","",""
 
 #推送信息
-def send_message(to_user, access_token, city_name, weather, max_temperature, min_temperature, pipi, lizhi, pop, tips, note_en, note_ch, health_tip, lucky_):
+def send_message(to_user, access_token, city_name, weather, max_temperature, min_temperature, pipi, lizhi, pop, tips,real,wind,sunrise,sunset,aqi,quality, note_en, note_ch, health_tip, lucky_):
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
     year = localtime().tm_year
